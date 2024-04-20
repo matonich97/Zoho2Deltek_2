55073339efc2e9250041fb9476751714ac9350b3
matonich97 <102882078+matonich97@users.noreply.github.com>
2024-04-20 08:42:06 -0500

    Add files via upload

---
+++ Release/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoho2Deltek_2.Release
+{
+    internal class Class1
+    {
+    }
+}
